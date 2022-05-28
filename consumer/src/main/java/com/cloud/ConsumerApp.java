be4e025122f41622fe529c1f80a09a96b35db015
buhaizhuang123 <buhaizhuang123@163.com>
2022-05-28 10:12:44 +0800

    11

--- consumer/src/main/java/com/cloud/ConsumerApp.java
+++ consumer/src/main/java/com/cloud/ConsumerApp.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author haizhuangbu
@@ -14,10 +16,17 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@RestController
 public class ConsumerApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConsumerApp.class,args);
+        SpringApplication.run(ConsumerApp.class, args);
     }
+
+    @RequestMapping("/")
+    public String defaultPage() {
+        return "Hello Default";
+    }
+
 
 }
