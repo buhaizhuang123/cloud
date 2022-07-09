a634bac0bff5681b8f97babca347701d08e83378
buhaizhuang123 <buhaizhuang123@163.com>
2022-07-09 21:51:14 +0800

    添加消息驱动及excel下载功能

--- product/src/main/java/com/cloud/person/controller/PsController.java
+++ product/src/main/java/com/cloud/person/controller/PsController.java
@@ -5,6 +5,10 @@ import com.cloud.person.dto.PersonDto;
 import com.cloud.person.dto.PersonVo;
 import com.cloud.person.service.PsService;
 import com.cloud.shop.dto.Page;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.elasticsearch.action.DocWriteResponse;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.slf4j.Logger;
@@ -13,6 +17,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
@@ -41,7 +47,7 @@ public class PsController {
     }
 
     @RequestMapping(value = "search", method = RequestMethod.POST)
-    public List<PersonDto> findPersons(@RequestBody PersonVo personDto,Page page) throws IOException {
+    public List<PersonDto> findPersons(@RequestBody PersonVo personDto, Page page) throws IOException {
         List<PersonDto> person = psService.findPerson(personDto, page);
         return person;
     }
@@ -53,9 +59,39 @@ public class PsController {
     }
 
     @RequestMapping("del")
-    public DeleteResponse del(@RequestParam("id")String id){
+    public DeleteResponse del(@RequestParam("id") String id) {
         logger.info("======== 删除 ============");
         return psService.delToPerson(id);
+    }
+
+    @RequestMapping("/list1")
+    public List<Long> getList1(@RequestBody List<Long> list) {
+        return list;
+    }
+
+
+    @RequestMapping("/list2")
+    public List<Long> getList2(List<Long> list) {
+        return list;
+    }
+
+    @RequestMapping("show")
+    public void showExcel(HttpServletResponse response) throws IOException {
+        response.setHeader("Content-Disposition","attachment;filename=123.xls");
+        // 预览
+//        response.setContentType("application/octet-stream");
+        // 下载
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        ServletOutputStream outputStream = response.getOutputStream();
+        HSSFWorkbook hssfWorkbook = new HSSFWorkbook();
+        HSSFSheet morn = hssfWorkbook.createSheet("morn");
+        HSSFRow row = morn.createRow(0);
+        for (int i = 0; i < 10; i++) {
+            HSSFCell cell = row.createCell(i);
+            cell.setCellValue(i);
+        }
+        hssfWorkbook.write(outputStream);
+        outputStream.close();
     }
 
 
